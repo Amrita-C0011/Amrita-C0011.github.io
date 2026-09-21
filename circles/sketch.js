0ba9ba07bf1a38cf6547843d45f8b3e37026a680
Amrita C. <7009919@spsd.sk.ca>
2026-09-21 15:14:59 -0600

    random colorful circles demo

---
+++ circles/sketch.js
@@ -0,0 +1,19 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+async function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // background(220);
+  fill ( random (255), random (255), random ( 255));
+  noStroke();
+  circle (random(0,width),random (0, height), random(30,100));
+
+}
